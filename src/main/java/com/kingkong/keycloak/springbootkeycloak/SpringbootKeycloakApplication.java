415ef6196299b801cc338baecd73c7a9b988615d
kingkongapps <kingkongapps@gmail.com>
2024-05-06 19:46:33 +0900

    springboot-keycloak 연동 샘플 소스...

--- src/main/java/com/kingkong/keycloak/springbootkeycloak/SpringbootKeycloakApplication.java
+++ src/main/java/com/kingkong/keycloak/springbootkeycloak/SpringbootKeycloakApplication.java
@@ -2,6 +2,8 @@ package com.kingkong.keycloak.springbootkeycloak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootKeycloakApplication {
@@ -10,4 +12,9 @@ public class SpringbootKeycloakApplication {
 		SpringApplication.run(SpringbootKeycloakApplication.class, args);
 	}
 
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
---
+++ src/main/java/com/kingkong/keycloak/springbootkeycloak/dao/CustomerDAO.java
@@ -0,0 +1,7 @@
+package com.kingkong.keycloak.springbootkeycloak.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerDAO extends CrudRepository<Customer, Long> {
+
+}
